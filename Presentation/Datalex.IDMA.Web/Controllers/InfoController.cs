3ef32edb597e4b79bd85f7a24455b4a62f40c7fd
Ryan Estes <ryan.estes@Datalex.com>
2016-06-22 13:51:58 +0100

    no message

---
+++ Presentation/Datalex.IDMA.Web/Controllers/InfoController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+namespace Datalex.IDMA.Web.Controllers
+{
+    /// <summary>
+    /// Represents the info controller
+    /// </summary>
+    public class InfoController : Controller
+    {
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
