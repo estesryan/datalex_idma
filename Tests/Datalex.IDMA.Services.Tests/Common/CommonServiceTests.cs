38a4ec6fc786f587458f7a4c5fde2eaf155d88b9
Ryan Estes <ryan.estes@Datalex.com>
2016-06-24 14:39:22 +0100

    Created Unit Tests for CommonService

---
+++ Tests/Datalex.IDMA.Services.Tests/Common/CommonServiceTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Datalex.IDMA.Services.Tests.Common
+{
+    [TestClass]
+    public class CommonServiceTests
+    {
+    }
+}
