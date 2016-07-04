b5568bb7417ad60838ecf5dc21dfdc194b66901f
Ryan Estes <ryan.estes@Datalex.com>
2016-07-04 09:09:01 +0100

    Added Health Check Result 2 domain class.
    Setup Health Check Result 2 Domain
    
    Added Comments to Health Check Result 2 Domain

---
+++ Libraries/Datalex.IDMA.Services/Common/HealthCheckResult2.cs
@@ -0,0 +1,9 @@
+﻿namespace Datalex.IDMA.Services.Common
+{
+    /// <summary>
+    /// Represents a health check result
+    /// </summary>
+    public partial class HealthCheckResult2
+    {
+    }
+}
